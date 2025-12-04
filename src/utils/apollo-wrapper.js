048e9a627996972bed94d5a36543d8e93ad7d0a3
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2025-12-04 19:53:40 +0100

    chore: Update NextJS, React, other deps, fix RSC CVE (#60)
    ]

--- src/utils/apollo-wrapper.js
+++ src/utils/apollo-wrapper.js
@@ -1,31 +1,31 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { ApolloLink, HttpLink } from '@apollo/client';
+import React from "react";
+import { ApolloLink, HttpLink } from "@apollo/client";
 import {
+	ApolloClient,
 	ApolloNextAppProvider,
-	NextSSRInMemoryCache,
+	InMemoryCache,
 	SSRMultipartLink,
-	NextSSRApolloClient,
-} from '@apollo/experimental-nextjs-app-support/ssr';
+} from "@apollo/client-integration-nextjs";
 
 function makeClient() {
 	const httpLink = new HttpLink({
-		uri: 'https://main--spacex-l4uc6p.apollographos.net/graphql',
+		uri: "https://main--spacex-l4uc6p.apollographos.net/graphql",
 		headers: {
 			Authorization: `Bearer 0591afcf9fb02880221a183e691ba3`,
 		},
 	});
 
-	return new NextSSRApolloClient({
-		cache: new NextSSRInMemoryCache(),
+	return new ApolloClient({
+		cache: new InMemoryCache(),
 		link:
-			typeof window === 'undefined'
+			typeof window === "undefined"
 				? ApolloLink.from([
-						new SSRMultipartLink({
-							stripDefer: true,
-						}),
-						httpLink,
+					new SSRMultipartLink({
+						stripDefer: true,
+					}),
+					httpLink,
 				])
 				: httpLink,
 	});
